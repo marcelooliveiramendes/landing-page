d83d6db71912d1a6a72294cf422e43b35831db4a
marcelooliveiramendes <marcelooliveiraah09@gmail.com>
2023-10-30 15:19:41 -0300

    feature: changed styles

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ const openModal = (code) => {
     console.log(code);
     var conteudo = `
       <iframe id="ytplayer" type="text/html" width="640" height="360"
-        src="http://www.youtube.com/embed/${code}?autoplay=1"
+        src="https://www.youtube.com/embed/${code}?autoplay=1"
         frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
     ></iframe>
     `
